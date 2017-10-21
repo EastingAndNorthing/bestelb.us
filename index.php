24480111ee53c96bd7e4a39400bdc61bc8a9786f
Mark <markeasting@gmail.com>
2017-10-21 19:51:15 +0200

    Added redirect headers + meta http-equiv fallback

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Hard coded for for simplicity's sake.
+// TODO: some kind of bijection function to translate urls back and forth
+
 $links = parse_ini_file('links.ini');
 
 $_SERVER['REQUEST_URI_PATH'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -9,6 +12,7 @@ $requested = end(array_filter($url_parts));
 
 if(isset($requested) && array_key_exists($requested, $links)){
 	$destination = $links[$requested];
+	header( "refresh:2;url=$destination" );
 }
 
 ?>
@@ -19,6 +23,9 @@ if(isset($requested) && array_key_exists($requested, $links)){
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php if($destination): ?>
+		<meta http-equiv="refresh" content="2;url=<?= $destination ?>">
+	<?php endif; ?>
 
 	<title>Bestelb.us</title>
 	<meta name="author" content="Mark Oosting">
@@ -37,11 +44,12 @@ if(isset($requested) && array_key_exists($requested, $links)){
 		<img class="logo" src="./assets/img/delivery-truck.svg" alt="Delivery truck">
 	</div>
 
-	<div class="aesthetic text-center">
+	<div class="text-center">
 		<?php if($destination): ?>
-			<p>Delivering you to: <p><a href="<?= $destination ?>"><?= parse_url($destination, PHP_URL_HOST) ?></a>
+			<p class="aesthetic">Delivering you to: <p><a href="<?= $destination ?>"><?= parse_url($destination, PHP_URL_HOST) ?></a>
 		<?php else: ?>
-			<p>Link not found.</p>
+			<p class="aesthetic">Bestelb.us</p>
+			<p>&copy; Mark Oosting, 2017</p>
 		<?php endif; ?>
 	</div>
 
